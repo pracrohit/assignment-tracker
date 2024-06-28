96cd890d1ae1dc0969330e43050fcdc4bfd88dc7
PraveenGPK1206 <kumargundrala@gmail.com>
2024-06-28 22:51:34 +0530

    assignments added

---
+++ Feedback_form/Feedback_form/Feedback_formApp.swift
@@ -0,0 +1,17 @@
+//
+//  Feedback_formApp.swift
+//  Feedback_form
+//
+//  Created by Praveen on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Feedback_formApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
